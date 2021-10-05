47afc6aa2103005dd9aaceabc347abe81a0a3a97
Вячеслав Вольхин <vaceslavvolhin@MacBook-Pro-Vaceslav.local>
2021-10-05 08:44:02 +0500

    start

---
+++ Gruntfile.js
@@ -0,0 +1,157 @@
+module.exports = function (grunt) {
+    /*const mozjpeg = require('imagemin-mozjpeg');*/
+    grunt.initConfig({
+
+        fixturesPath: "_includes",
+
+
+
+
+        htmlbuild: {
+            dist: {
+                src: './_html/*.html',
+                dest: './',
+                options: {
+                    beautify: true,
+                    sections: {
+                        scripts: '<%= fixturesPath %>/**/scripts.html',
+                        head: '<%= fixturesPath %>/**/head.html',
+                        header: '<%= fixturesPath %>/**/header.html',
+                        footer: '<%= fixturesPath %>/**/footer.html',
+                    },
+                    data: {
+                        version: "0.1.0",
+                        title: "test",
+                    },
+                }
+            }
+        },
+
+        imagemin: {
+            /*static: {
+                options: {
+                    optimizationLevel: 3,
+                    svgoPlugins: [{removeViewBox: false}],
+                },
+                files: {
+                    'img/!*.png': 'img/!*.png',
+                    'img/!*.jpg': 'img/!*.jpg',
+                    'img/!*.gif': 'img/!*.gif',
+                    'img/!*.svg': 'img/!*.svg',
+                }
+            },*/
+            dynamic: {
+                options: {
+                    optimizationLevel: 6,
+                    svgoPlugins: [{removeViewBox: false}],
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'img/',
+                    src: ['**/*.{png,jpg,gif, svg}'],
+                    dest: 'img/'
+                }]
+            }
+        },
+
+        sprite: {
+            all: {
+                src: 'img/spr/*.png',
+                dest: 'img/spr.png',
+                destCss: 'css/less/icons.less',
+                padding: 20,
+                imgPath: '../img/spr.png',
+                cssFormat: 'css',
+                cssOpts: {
+                    'functions': false,
+
+                    'cssClass': function (item) {
+                        return '.i-' + item.name;
+                    }
+                },
+                algorithm: 'binary-tree'
+            }
+        },
+
+        less: {
+            development: {
+                options: {
+                    compress: false,
+                    yuicompress: false,
+                    optimization: 2
+                },
+                files: {
+                    "css/style.css": "css/less/style.less",
+                    "css/style.min.css": "css/less/style.less"
+                }
+            }
+        },
+
+
+        postcss: {
+            min: {
+                options: {
+                    /*map: {
+                        inline: false,
+                        annotation: 'css/maps/'
+                    },*/
+                    processors: [
+                        require('pixrem')(), // add fallbacks for rem units
+                        require('autoprefixer')({overrideBrowserslist: 'last 2 versions'}), // add vendor prefixes
+                        require('cssnano')({
+                            options: {
+                                sourcemap: false
+                            },
+                            dist: {
+                                files: {
+                                    "css/style.min.css": "css/style.min.css"
+                                }
+                            }
+                        })
+                    ]
+                },
+                src: 'css/style.min.css',
+                dist: 'css/style.min.css',
+            },
+            style: {
+                options: {
+                    /*map: {
+                        inline: false,
+                        annotation: 'css/maps/'
+                    },*/
+                    processors: [
+                        require('pixrem')(), // add fallbacks for rem units
+                        require('autoprefixer')({overrideBrowserslist: 'last 2 versions'}),
+                    ]
+                },
+                src: 'css/style.css',
+                dist: 'css/style.css',
+            }
+        },
+
+
+
+        watch: {
+            html: {
+                files: ['_includes/*.html', '_html/*.html'],
+                tasks: 'htmlbuild'
+            },
+            sprite: {
+                files: ['img/spr/*.png'],
+                tasks: 'sprite'
+            },
+            styles: {
+                files: ['css/less/*.less'],
+                tasks: ['less', 'postcss'],
+                options: {
+                    nospawn: true
+                }
+            },
+        }
+    });
+
+    require('load-grunt-tasks')(grunt);
+    require('postcss-plugin')({});
+
+    grunt.registerTask('default', ['watch'], ['less'], ['sprite'],  ['htmlbuild'], ['postcss'], ['imagemin']);
+};
---
+++ js/scripts.js
@@ -0,0 +1,106 @@
+$(document).ready(function(){
+
+	//phone masked
+	$('input[type="tel"]').mask("+7 (999) 999-99-99",{placeholder:"+7 (___) ___-__-__"});
+	$('input[type="tel"]').on('click', function() {
+		$(this).setCursorPosition(4);
+	})
+	$.fn.setCursorPosition = function(pos) {
+	  this.each(function(index, elem) {
+	    if (elem.setSelectionRange) {
+	      elem.setSelectionRange(pos, pos);
+	    } else if (elem.createTextRange) {
+	      var range = elem.createTextRange();
+	      range.collapse(true);
+	      range.moveEnd('character', pos);
+	      range.moveStart('character', pos);
+	      range.select();
+	    }
+	  });
+	  return this;
+	};
+
+	//popup block
+	$('.js-popup-wrap .js-btn-toggle').on('click', function() {
+		if ($(this).hasClass('active')) {
+			$(this).removeClass('active');
+			$('body').removeClass('menu-show');
+		} else {
+			$('.js-popup-wrap:not(.no-close) .js-btn-toggle').removeClass('active');
+			$(this).addClass('active');
+			if ($(this).parent().hasClass('main-menu-wrap')) {
+				$('body').addClass('menu-show');
+			}
+		}
+		return false;
+	})
+	$('.js-popup-wrap .js-btn-close').on('click', function() {
+		$(this).parents('.js-popup-wrap').children('.js-btn-toggle').removeClass('active');
+		$('body').removeClass('menu-show');
+		return false;
+	})
+	$(document).click(function(event) {
+	    if ($(event.target).closest(".js-popup-block").length) return;
+	    $('.js-popup-wrap:not(.no-close) .js-btn-toggle').removeClass('active');
+	    $('body').removeClass('menu-show');
+	    event.stopPropagation();
+	});
+	$('.js-popup-wrap').each(function() {
+		if ($(this).hasClass('js-popup-select')) {
+			// alert(1)
+			if ($(this).find('.js-popup-block').find('.active').length>0) {} else {
+				$(this).find('.js-popup-block').find('li').eq(0).children('a').addClass('active');
+			}
+			var currentSelect = $(this).find('.js-popup-block').find('.active').html();
+			$(this).find('.js-btn-toggle').html(currentSelect);
+		}
+	})
+	$('.js-popup-wrap.js-popup-select .js-popup-block a').on('click', function() {
+		if ($(this).hasClass('active')) {} else {
+			$(this).parents('.js-popup-wrap').find('.js-popup-block').find('.active').removeClass('active');
+			$(this).addClass('active');
+		}
+		$('.js-popup-wrap').each(function() {
+			if ($(this).hasClass('js-popup-select')) {
+				if ($(this).find('.js-popup-block').find('.active').length>0) {} else {
+					$(this).find('.js-popup-block').find('li').eq(0).children('a').addClass('active');
+				}
+				var currentSelect = $(this).find('.js-popup-block').find('.active').html();
+				$(this).find('.js-btn-toggle').html(currentSelect);
+			}
+		})
+		$(this).parents('.js-popup-wrap').find('.js-btn-toggle').removeClass('active');
+		return false;
+	})
+
+	//tabs
+	$('.js-tabs-nav').each(function() {
+		$('.js-tab-block[data-tab*="'+$(this).find('.active').attr('data-tab')+'"]').addClass('active');
+	})
+	$('.js-tab-title').each(function() {
+		if ($(this).hasClass('active')) {
+			$(this).next('.js-tab-content').show(0);
+		}
+	})
+	$('.js-tabs-nav li a').on('click', function() {
+		if ($(this).hasClass('active')) {} else {
+			$('.js-tab-block').removeClass('active');
+			$(this).parents('.js-tabs-nav').find('.active').removeClass('active');
+			$(this).addClass('active');
+			$('.js-tabs-nav').each(function() {
+				$('.js-tab-block[data-tab*="'+$(this).find('.active').attr('data-tab')+'"]').addClass('active');
+			})
+		}
+		return false;
+	})
+	$('.js-tab-title').on('click' , function() {
+		if ($(this).hasClass('active')) {
+			$(this).removeClass('active').next('.js-tab-content').slideUp(200);
+		} else {
+			$(this).addClass('active').next('.js-tab-content').slideDown(200);
+		}
+	})
+	
+});
+
+
